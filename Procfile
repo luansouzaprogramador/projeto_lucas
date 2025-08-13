4c961983201f812c4902d1db88685ffe6e04f87a
Luan Marcelino de Souza <luansouzaa.ti@gmail.com>
2025-08-13 11:34:54 -0300

    update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn codigo:app
+web: gunicorn app:app
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="meu_chamados_app",
+    version="0.1",
+    packages=find_packages(),
+)
